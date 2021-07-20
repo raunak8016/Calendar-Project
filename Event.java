75a6c2b75d854cbdb19ae733504cca0fb962abb4
siddsp <sidd.pai@ucalgary.ca>
2021-07-20 00:41:10 -0600

    Added basic functions and structure to Event class

--- Event.java
+++ Event.java
@@ -1,12 +1,114 @@
+/**
+ * 
+ * @author Sidd
+ *
+ */
 
 public class Event {
 	private String eventName;
 	private int eventYear;
 	private int eventDay;
+	private double[] eventTimeFrame = new double[] {0.0, 24.0};
 	
-	//create constructor with all three arguments
+	public Event(String eventName, int eventYear, int eventDay) {
+		this.eventName = eventName;
+		this.eventYear = eventYear;
+		this.eventDay = eventDay;
+	}
 	
-	//create getter and setter methods for all three variables
+	public Event(String eventName, int eventYear, int eventDay, double[] eventTimeFrame) {
+		this.eventName = eventName;
+		this.eventYear = eventYear;
+		this.eventDay = eventDay;
+		if (eventTimeFrame[0] < eventTimeFrame[1] && eventTimeFrame.length == 2) {
+			if (eventTimeFrame[0] >= 0.0 && eventTimeFrame[1] <= 24.0) {
+				this.eventTimeFrame = eventTimeFrame;
+			}
+		}
+	}
+
+	// Getter and setter methods for eventName.
 	
-	//create to string method
+	/**
+	 * @return the eventName
+	 */
+	public String getEventName() {
+		return eventName;
+	}
+	
+	/**
+	 * @return the eventName
+	 */
+	public Event[] getEventNamesByName(String name) {
+		return new Event[0];
+	}
+	
+	/**
+	 * @return the eventName
+	 */
+	public Event[] getEventNamesByDate(String date) {
+		return new Event[0];
+	}
+	
+	/**
+	 * @param eventName the eventName to set
+	 */
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
+	
+	// Getter and setter methods for eventYear.
+	
+	/**
+	 * @return the eventYear
+	 */
+	public int getEventYear() {
+		return eventYear;
+	}
+
+	
+	/**
+	 * @param eventYear the eventYear to set
+	 */
+	public void setEventYear(int eventYear) {
+		this.eventYear = eventYear;
+	}
+	
+	// Getter and setter methods for eventDay.
+	
+	/**
+	 * @return the eventDay
+	 */
+	public int getEventDay() {
+		return eventDay;
+	}
+	
+	/**
+	 * @param eventDay the eventDay to set
+	 */
+	public void setEventDay(int eventDay) {
+		this.eventDay = eventDay;
+	}
+	
+	// Getter and setter methods for eventTimeFrame.
+	
+	/**
+	 * @return the eventTimeFrame
+	 */
+	public double[] getEventTimeFrame() {
+		return eventTimeFrame;
+	}
+
+	/**
+	 * @param eventTimeFrame the eventTimeFrame to set
+	 */
+	public void setEventTimeFrame(double[] eventTimeFrame) {
+		this.eventTimeFrame = eventTimeFrame;
+	}
+	
+	// toString() method.
+
+	public String toString() {
+		return String.format("");
+	}
 }
