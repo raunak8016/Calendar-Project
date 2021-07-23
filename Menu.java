9c3e8c37ed5fe9cc4d17e7678bb569492c65f681
siddsp <sidd.pai@ucalgary.ca>
2021-07-22 18:45:23 -0600

    added Menu and Main class for application

---
+++ Menu.java
@@ -0,0 +1,13 @@
+
+public class Menu {
+
+	public Menu() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
