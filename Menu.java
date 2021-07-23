082870b7515ce699ed615ac29aefdf838a0d97c6
siddsp <sidd.pai@ucalgary.ca>
2021-07-22 20:09:22 -0600

    added menu interface with options

--- Menu.java
+++ Menu.java
@@ -1,13 +1,42 @@
+/**
+ * 
+ * @author Sidd
+ *
+ */
 
-public class Menu {
+public class Menu extends Interface{
+	
+	public void callMenu() {
+		System.out.println(this);
+		int a = 10;
+		while (a != 0) {
+			switch (a) {
+				case 0:
+					System.out.println("0");
+					break;
+				case 1:
+					System.out.println("1");
+					break;
+				case 2:
+					System.out.println("2");
+					break;
+				case 3:
+					System.out.println("3");
+					break;
+				default:
+					System.out.println("x");
+					System.exit(0);
+			}
+		}
+	}
 
-	public Menu() {
-		// TODO Auto-generated constructor stub
+	public String toString() {
+		return "xyz";
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Menu menu = new Menu();
+		menu.run();
 	}
 
 }
