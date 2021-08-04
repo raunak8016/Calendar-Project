dadd7643f8a060a225f7a160c3d2e54cecf1b668
grunc10161122 <86758419+grunc10161122@users.noreply.github.com>
2021-08-03 23:03:49 -0600

    Added StartView, made major changes to MainView and StartView controllers.  Will see in pull request later

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,8 @@
 module fxtemplate {
 	requires javafx.controls;
 	requires javafx.graphics;
+	requires javafx.fxml;
+	requires javafx.base;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
