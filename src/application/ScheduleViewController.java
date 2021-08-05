08fcc8f6a9434c9138054d3f14459397e6b313d4
siddsp <sidd.pai@ucalgary.ca>
2021-08-05 15:33:04 -0600

    Added functionality to the ScheduleView in GUI.

--- src/application/ScheduleViewController.java
+++ src/application/ScheduleViewController.java
@@ -1,14 +1,39 @@
-/**
- * 
- */
 package application;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import models.Event;
+import models.Schedule;
+
 /**
- * @author Sidd University
+ * 
+ * @author Sidd
  *
  */
 public class ScheduleViewController {
+	Schedule schedule;
 
-	
+    @FXML
+    private ResourceBundle resources;
 
+    @FXML
+    private URL location;
+
+    @FXML
+    private ListView<Event> eventListView;
+
+    @FXML
+    void initialize() {
+        assert eventListView != null : "fx:id=\"eventListView\" was not injected: check your FXML file 'ScheduleView.fxml'.";
+        
+        this.schedule = new Schedule(2030, 9, 22);
+        double[] timeFrameTest = {6.0, 12.0};
+        this.schedule.addEvent(new Event("ABC", 22, 9, 2030, timeFrameTest));
+        
+        eventListView.setItems(FXCollections.observableArrayList(this.schedule.getEvents()));
+    }
 }
--- src/application/ScheduleViewTester.java
+++ src/application/ScheduleViewTester.java
@@ -1,15 +1,29 @@
 package application;
 
+import java.io.FileInputStream;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class ScheduleViewTester extends Application {
-
+	
 	@Override
 	public void start(Stage primaryStage) {
-		
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			BorderPane root = loader.load(new FileInputStream("src/views/ScheduleView.fxml"));
+			Scene scene = new Scene(root, 500, 500);
+			
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
